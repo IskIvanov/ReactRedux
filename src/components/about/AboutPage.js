5f94c2d43ceee190316247f966a0df45bb589927
Iskren <iskren.ivanov@accenture.com>
2019-05-21 18:54:09 +0300

    Fix

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import {Link} from "react-router-dom"
 
 const AboutPage = () =>(
-	<div className="jumbotron">
-		<h1>Administration</h1>
-		<p>React, Redux and React Router for ultra-responsive web apps.</p>
-		<Link to="about" className="btn btn-primary btn-lg">
-			Learn More		
-		</Link>
+	<div>
+		<h2>About</h2>
+		<p>
+			This App uses react and redux and many more helpful libraries
+		</p>
 	</div>
 )
 
