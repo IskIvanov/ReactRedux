c49786ea675927b12008ee838a0a36f4c1741b20
Iskren <iskren.ivanov@accenture.com>
2019-05-21 18:50:31 +0300

    first commit

---
+++ src/components/home/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () =>(
+	<div>
+		<h2>About</h2>
+		<p>
+			This App uses react and redux and many more helpful libraries
+		</p>
+	</div>
+)
+
+export default HomePage;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
+import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(
+	<Router>
+		<App />
+	</Router>
+	, document.getElementById('root'));
+
 serviceWorker.unregister();
